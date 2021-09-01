8b28758ac85affc5967725d30f8a3ecc76cb5063
CST1229 <68464103+CST1229@users.noreply.github.com>
2021-09-01 19:05:51 +0200

    Add extension of join blocks

---
+++ extensions/extensionOfJoinBlocks.js
@@ -0,0 +1,293 @@
+class JoinBlocks {
+  getInfo () {
+    return {
+      id: 'joinBlocks',
+      name: 'Join Blocks',
+      blocks: [
+        {
+          opcode: 'join3',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join4',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join5',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4] [5]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			5: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join6',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4] [5] [6]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			5: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			6: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join7',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4] [5] [6] [7]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			5: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			6: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			7: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join8',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4] [5] [6] [7] [8]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			5: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			6: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			7: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			8: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join9',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4] [5] [6] [7] [8] [9]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			5: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			6: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			7: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			8: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			9: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        },
+		{
+          opcode: 'join10',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'JOIN [1] [2] [3] [4] [5] [6] [7] [8] [10]',
+          arguments: {
+            1: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			2: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			3: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			4: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			5: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			6: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			7: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			8: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			9: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            },
+			10: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'text'
+            }
+          }
+        }
+      ]
+    };
+  }
+  
+  joinLots(args) {
+    let text = "";
+	for (each arg in args) {
+	  text += arg;
+	}
+	return text;
+  }
+  
+  join3(args) {joinLots(args);}
+  join4(args) {joinLots(args);}
+  join5(args) {joinLots(args);}
+  join6(args) {joinLots(args);}
+  join7(args) {joinLots(args);}
+  join8(args) {joinLots(args);}
+  join9(args) {joinLots(args);}
+  join10(args) {joinLots(args);}
+}
+
+Scratch.extensions.register(new JoinBlocks());
