9438d43c0cd30c814d34fab78afd8523b6cef1ea
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-06-04 18:02:27 +0200

    shellfishpfps 0.9.1
    - removed irccloud pfp support
    - userscript delay
    - the pfp now updates live when you change the setting
    - hopefully fixed some bugs

--- userscripts/shellfishpfps.user.js
+++ userscripts/shellfishpfps.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Shellfish PFPs
 // @namespace    https://cst1229.github.io/
-// @version      0.9
-// @description  Shows IRCCloud PFPs and allows setting a PFP in shellfish (Kiwi IRC). To set your PFP, set shellfishpfps.pfp in the advanced settings to an image URL. In beta, stuff might break.
+// @version      0.9.1
+// @description  Allows setting a PFP in shellfish (Kiwi IRC). To set your PFP, set shellfishpfps.pfp in the advanced settings to an image URL. In beta, stuff might break.
 // @author       CST1229
 // @match        https://web.libera.chat/*
 // @match        https://kiwiirc.com/nextclient/*
@@ -12,50 +12,21 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+window.setTimeout(function() {
     'use strict';
 	const kiwi = window.kiwi;
+	
+	if (!kiwi) return;
 	
 	if (kiwi.state.getSetting("user_settings.shellfishpfps.pfp") === undefined) {
 		kiwi.state.setSetting("user_settings.shellfishpfps.pfp", "");
 	}
-	// irccloud avatars. pasted from kiwiirc docs lol
-	document.body.appendChild(Object.assign(
-		document.createElement("div"),
-		{
-			id: "irccloud_avatars",
-			style: "width:1px;height:1px;position:absolute;left:-1000px;"
-		}
-	));
+	
+	let pfpSetting = kiwi.state.getSetting("user_settings.shellfishpfps.pfp");
+	
 	kiwi.on('irc.userlist', function(event, network) {
-		const scratch = document.querySelector('#irccloud_avatars');
-
 		event.users.forEach(function(user) {
-			// first, ask the user for their pfp through ctcp
 			network.frameworkClient.ctcpRequest(user.nick, "PFP");
-			
-			const m = user.ident.match(/^uid|sid([0-9]+)$/);
-			if (m) {
-				const avatarUrl = 'https://static.irccloud-cdn.com/avatar-redirect/' + m[1];
-				const img = document.createElement('img');
-				img.src = avatarUrl;
-
-				// irccloud replies with a 404 if an avatar doesn't exist, so this onload callback
-				// will only be called for successful avatars
-				img.onload = function() {
-					kiwi.state.addUser(network.id, {nick: user.nick, avatar: {
-						small: avatarUrl,
-					}});
-
-					scratch.removeChild(img);
-				};
-
-				img.onerror = function() {
-					scratch.removeChild(img);
-				};
-
-				scratch.appendChild(img);
-			}
 		});
 	});
 
@@ -67,9 +38,7 @@
 			
 			// someone sent the pfp to the client by themselves
 			// instead of responding to a request
-			kiwi.state.addUser(network.id, {nick: event.nick, avatar: {
-				small: returnVal,
-			}});
+			addPfp(network.id, event.nick, returnVal);
 		} catch (e) {}
 		
 		let target = event.from_server ?
@@ -79,8 +48,8 @@
 		const pfp = kiwi.state.getSetting("user_settings.shellfishpfps.pfp");
 		let pfpUrl = "";
 		if (pfp) pfpUrl = pfp;
-
-		network.frameworkClient.ctcpResponse(target, "PFP", pfp);
+		
+		sendPfp(network, true, target, pfp);
 	});
 	kiwi.on("irc.ctcp response", function(event, network, ircEventObj) {
 		if (event.type !== "PFP") return;
@@ -91,10 +60,8 @@
 		} catch (e) {
 			return;
 		}
-
-		kiwi.state.addUser(network.id, {nick: event.nick, avatar: {
-			small: returnVal,
-		}});
+		
+		addPfp(network.id, event.nick, returnVal);
 	});
 	kiwi.on("network.connecting", function(connectingEvent) {
 		const pfp = kiwi.state.getSetting("user_settings.shellfishpfps.pfp");
@@ -105,12 +72,46 @@
 			return;
 		}
 		
-		kiwi.on("irc.connected", function(connectedEvent) {
-			kiwi.state.addUser(connectingEvent.network.id, {nick: connectedEvent.nick, avatar: {
-				small: pfp,
-			}});
+		kiwi.once("irc.connected", function(connectedEvent) {
+			addPfp(connectingEvent.network.id, connectingEvent.network.nick, pfp);
 		});
 	});
 	
+	function sendPfp(network, isReply, sendTo, pfp) {
+		if (sendTo === network.nick) {
+			addPfp(network.id, network.nick, pfp);
+			return;
+		}
+		
+		if (isReply) {
+			network.frameworkClient.ctcpResponse(sendTo, "PFP", ((pfp || "-")));
+		} else {
+			network.frameworkClient.ctcpRequest(sendTo, "PFP", ((pfp || "-")));
+		}
+	}
+	function addPfp(networkId, nick, pfp) {
+		try {
+			new URL(pfp);
+			kiwi.state.addUser(networkId, {nick: nick, avatar: {
+				small: pfp,
+			}});
+		} catch (e) {
+			kiwi.state.addUser(networkId, {nick: nick, avatar: null});
+		}
+	}
+	
+	setInterval(() => {
+		const kiwi = window.kiwi;
+		
+		if (pfpSetting !== kiwi.state.getSetting("user_settings.shellfishpfps.pfp")) {
+			pfpSetting = kiwi.state.getSetting("user_settings.shellfishpfps.pfp");
+			for (const network of kiwi.state.networks) {
+				for (const user in network.users) {
+					sendPfp(network, false, network.users[user].nick, pfpSetting);
+				}
+			}
+		}
+	}, 1000);
+	
 	console.log("pfps thing started", kiwi);
-})();
+}, 1000);
