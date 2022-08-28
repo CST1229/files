beadb4e337235c1cf716faac4a24a06f598c80b2
CST1229 <68464103+CST1229@users.noreply.github.com>
2022-08-28 02:31:51 +0200

    i hope it works

---
+++ extensions/epicques-load-stuff-from-url.js
@@ -0,0 +1,20 @@
+vm.runtime.once("PROJECT_LOADED", async function() {
+  const searchParams = new URL(location.href).searchParams;
+  const projectUrl = searchParams.get("project_url");
+  let extsToLoad = [];
+  searchParams.forEach(function(value, name) {
+    if (name === "unsandboxed_extension") {
+      extsToLoad.push(value);
+    }
+  });
+  const extsLoad = Promise.all(extsToLoad.map(url => fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`)));
+  const extsText = Promise.all((await extsLoad).map(resp => resp.text()));
+  
+  for (const script of await extsText) {
+    (new Function(script))();
+  };
+  
+  if (!projectUrl) return;
+  
+  vm.loadProject(await (await fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(projectUrl)}`)).blob());
+});
