b76a42128c60907e6b639c217a2ed36f54685364
CST1229 <68464103+CST1229@users.noreply.github.com>
2025-01-27 18:03:26 +0100

    scratch-fps-fix: v1.1

--- userscripts/scratch-fps-fix.user.js
+++ userscripts/scratch-fps-fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Scratch FPS Fix
 // @namespace    https://cst1229.github.io
-// @version      1.0.1
+// @version      1.1.0
 // @description  TRIES to fix consistent project lag (projects running at 22 FPS) on Firefox. Not very tested
 // @author       CST1229
 // @match        https://turbowarp.org/*
@@ -12,15 +12,42 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+// version 1.0: initial release
+// version 1.0.1: step only once per animation frame
+// version 1.1: add support for a fully vanilla environment (no addons or turbowarp)
+
+(async function() {
     'use strict';
 
 	let vm;
-	try {
-		vm = window?.vm || window?.__addon?.tab?.traps?.vm
-		if (!vm) return;
-	} catch(e) {
-		return;
+	// if no #app exists, this is definitely not a project player page
+	if (!document.querySelector("#app")) return;
+	// try to get the vm a bunch of times in case it takes some time to load
+	for (let vmTries = 20; vmTries > 0; vmTries--) {
+		try {
+			vm = window?.vm || window?.__addon?.tab?.traps?.vm || findVMFromDOM();
+			if (vm) break;
+		} catch(e) {
+			console.error("Scratch FPS Fix: Got error!", e);
+			continue;
+		}
+		await new Promise(res => setTimeout(res, 300));
+	}
+	if (!vm) return;
+	
+	// in fully vanilla Scratch scenarios
+	function findVMFromDOM(root = null) {
+		root ||= getReactInternalInstance(document.querySelector(".guiPlayer"))?.child?.sibling || getReactInternalInstance(document.querySelector(".gui"))?.return?.return?.return?.return;
+		if (!root) return null;
+		while (root) {
+			if (root?.stateNode?.props?.vm) return root.stateNode.props.vm;
+			root = root.child;
+		}
+		return null;
+	}
+	function getReactInternalInstance(el) {
+		if (!el) return null;
+		return el[Object.keys(el).find(o => o.startsWith("__reactInternalInstance")) || ""];
 	}
 	
 	const customInterval = Symbol();
